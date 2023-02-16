add8483aeb0002d6fd7bc449074cbf19b4d486df
jiseok419 <pyosik0419@gmail.com>
2023-02-16 19:20:36 +0900

    :tada:	:: 프로젝트 기초 설정

---
+++ src/main/java/com/dgsw/Daechelin/DaechelinApplication.java
@@ -0,0 +1,13 @@
+package com.dgsw.Daechelin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaechelinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaechelinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dgsw/Daechelin/DaechelinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dgsw.Daechelin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaechelinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

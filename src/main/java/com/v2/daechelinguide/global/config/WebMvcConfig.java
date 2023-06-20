c57cd6586c080ba954f0349884f8a0dfd2851c80
jiseok419 <pyosik0419@gmail.com>
2023-06-20 14:15:05 +0900

    :zap: :: cors 적용

---
+++ src/main/java/com/v2/daechelinguide/global/config/WebMvcConfig.java
@@ -0,0 +1,14 @@
+package com.v2.daechelinguide.global.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowCredentials(false);
+    }
+}
